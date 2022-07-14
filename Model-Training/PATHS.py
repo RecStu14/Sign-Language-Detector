13748ac5a2eb986d2d6ecf155a254ab34bff6bc2
RecStu14 <resources.study14@gmail.com>
2022-07-14 17:48:36 +0800

    updated paths

--- Model-Training/PATHS.py
+++ Model-Training/PATHS.py
@@ -2,8 +2,8 @@
 #Consolidated Paths that can be imported 
 
 #DATASET DIRECTORY
-DATASET_DIR = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Detector/American-Sign-Language-Letters"
-TRAIN = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Detector/American-Sign-Language-Letters/train"
-VALID = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Detector/American-Sign-Language-Letters/valid"
-TEST = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Detector/American-Sign-Language-Letters/test"
+DATASET_DIR = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition/American-Sign-Language-Letters"
+TRAIN = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition/American-Sign-Language-Letters/train"
+VALID = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition/American-Sign-Language-Letters/valid"
+TEST = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition/American-Sign-Language-Letters/test"
 
