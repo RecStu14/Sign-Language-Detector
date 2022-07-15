9a111e356a14f8380fe8e779a8340b095b497621
RecStu14 <resources.study14@gmail.com>
2022-07-15 16:56:10 +0800

    added new paths

--- Model-Training/PATHS.py
+++ Model-Training/PATHS.py
@@ -1,5 +1,8 @@
 #PATHS
 #Consolidated Paths that can be imported 
+import os
+
+ROOT_DIR = '/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition'
 
 #DATASET DIRECTORY
 DATASET_DIR = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition/American-Sign-Language-Letters"
@@ -7,3 +10,6 @@ TRAIN = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition/American
 VALID = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition/American-Sign-Language-Letters/valid"
 TEST = "/Users/sankeerthana/Documents/GitHub/Sign-Language-Recognition/American-Sign-Language-Letters/test"
 
+#MODEL TRAINING DIRECTORY
+MODEL_TRAINING = os.path.join(ROOT_DIR,'Model-Training')
+TRY1 = os.path.join(MODEL_TRAINING, 'try1-overfitting')
