62da537a522b95c5007be3061922b1d48073b122
RecStu14 <resources.study14@gmail.com>
2022-07-17 12:23:52 +0800

    added ref link

--- Model-Training/train_model.py
+++ Model-Training/train_model.py
@@ -5,17 +5,5 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import PATHS as PATHS
 
-#Processes records written in TFRecord Format
-raw_train_dataset = tf.data.TFRecordDataset(PATHS.TRAIN_TFR)
-
-# Create a dictionary describing the features.
-feature_description = {
-    'label': tf.io.FixedLenFeature([], tf.int64),
-    'features': tf.io.FixedLenFeature([], tf.string),
-}
-
-
-for images, labels in raw_train_dataset.take(1):  # only take first element of dataset
-    numpy_images = images.numpy()
-    numpy_labels = labels.numpy()
+#https://towardsdatascience.com/custom-object-detection-using-tensorflow-from-scratch-e61da2e10087
 
